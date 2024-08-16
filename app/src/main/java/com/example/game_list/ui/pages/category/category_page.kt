44d6b540a33326132091ae8af2901990b1140356
fontsignacio <fontsignacio@gmail.com>
2024-08-15 23:26:21 -0300

    LazyVerticalGrid by categories

--- app/src/main/java/com/example/game_list/ui/pages/category/category_page.kt
+++ app/src/main/java/com/example/game_list/ui/pages/category/category_page.kt
@@ -6,14 +6,22 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.game_list.enums.GameGenre
 import com.example.game_list.ui.theme.blueLight
 
 @Composable
 fun CategoryPage(paddingValues: PaddingValues){
+    val genres = GameGenre.entries.toList()
+
     Column(
         modifier = Modifier
             .padding(paddingValues)
@@ -21,7 +29,25 @@ fun CategoryPage(paddingValues: PaddingValues){
             .background(blueLight),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
-    ) { 
-        Text(text = "Categories")
+    ) {
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            modifier = Modifier.padding(8.dp)
+        ) {
+            items(genres) { genre ->
+                GameGenreItem(genre)
+            }
+        }
+    }
+}
+
+@Composable
+fun GameGenreItem(genre: GameGenre) {
+    Card(modifier = Modifier.padding(10.dp)) {
+        Text(
+            text = genre.displayName,
+            modifier = Modifier
+                .padding(16.dp)
+        )
     }
 }
